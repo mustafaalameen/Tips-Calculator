cbc1e94b1fccc8469d98ee2521fc3a2fce91b3af
mustafaumar2002@gmail.com <mustafaumar2002@gmail.com>
2023-03-05 12:45:32 +0100

    Finished everything necessary about the calculator

--- script.js
+++ script.js
@@ -4,46 +4,92 @@ For further developent, the custom input box and the rounding of values would be
 */
 const bill = document.querySelector(".bill-input");
 
-const calcTips = document.querySelectorAll(".tips-btn");
+const calcTipsBtns = document.querySelectorAll(".tips-btn");
 
 const tips_amount = document.querySelector(".tip-amount");
 
 const total_amount = document.querySelector(".total-amount");
 
-const reset = document.querySelector(".reset");
+const btnReset = document.querySelector(".reset");
 
-const numOfPeople = document.querySelector("#num");
+const numOfPeople = document.querySelector(".num");
 
-const displayCalc = function (bill, percent) {
-  tips_amount.textContent = `$${bill * percent}`;
-  total_amount.textContent = `$${bill * percent + bill}`;
+const numZeroDisplay = document.querySelector(".num-display");
+console.log(numZeroDisplay);
+const tipsInput = document.querySelector(".tips-input");
 
-  reset.classList.add("resetDOM");
+const valInputEl = document.querySelector("#input-box");
+
+
+
+// billDisplay.classList.remove("bill-zero");
+
+// bill.classList.remove("bill-zero");
+// numOfPeople.classList.add("num-zero");
+
+// function that calculate tips per person
+const calculateTip = function (tipsPercent) {
+  const tipsAmount = tipsPercent * Number(bill.value);
+  const tipsPerPerson = tipsAmount / Number(numOfPeople.value);
+
+  return tipsPerPerson;
 };
 
-reset.addEventListener("click", function () {
-  reset.classList.remove("resetDOM");
-  bill.value = 0;
-  tips_amount.textContent = `$0.00`;
-  total_amount.textContent = `$0.00`;
-  numOfPeople.textContent = Number(numOfPeople.textContent) + 1;
+//function to calculate the bill value
+const calculateBill = function (tipPerPerson) {
+  const totalPerPerson = Number(bill.value) / Number(numOfPeople.value);
+  return totalPerPerson + tipPerPerson;
+};
+
+// a function is needed
+const activateIfNumIsNotZero = function () {
+  numOfPeople.classList.remove("num-zero");
+  numZeroDisplay.classList.add("num-display");
+};
+
+activateIfNumIsNotZero();
+
+calcTipsBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const btnVal = btn.textContent;
+    const btnNumVal = Number(btnVal.slice(0, btnVal.indexOf("%"))) / 100;
+    const tipAmount = calculateTip(btnNumVal);
+    const totalBill = calculateBill(tipAmount);
+    if (Number(numOfPeople.value)) {
+      tips_amount.innerHTML = tipAmount
+        ? `$${tipAmount.toFixed(2)}`
+        : ` $${0.0}`;
+      total_amount.innerHTML = totalBill
+        ? `$${totalBill.toFixed(2)}`
+        : ` $${0.0}`;
+      activateIfNumIsNotZero();
+    } else {
+      numOfPeople.classList.toggle("num-zero");
+      numZeroDisplay.classList.toggle("num-display");
+    }
+  });
 });
 
-for (let i = 0; i < calcTips.length; i++) {
-  calcTips[i].addEventListener("click", function () {
-    if (calcTips[i].textContent === "5%") {
-      displayCalc(Number(bill.value), 0.05);
-    } else if (calcTips[i].textContent === "10%") {
-      displayCalc(Number(bill.value), 0.1);
-    } else if (calcTips[i].textContent === "15%") {
-      displayCalc(Number(bill.value), 0.15);
-    } else if (calcTips[i].textContent === "25%") {
-      displayCalc(Number(bill.value), 0.25);
-    } else if (calcTips[i].textContent === "50%") {
-      displayCalc(Number(bill.value), 0.5);
-    }
-    // else if (calcTips[5]){
-    //   console.log(bill.value * (calcTips[i].value)/ 100);
-    // }
-  });
-}
+valInputEl.addEventListener("change", function () {
+  const btnNumVal = Number(valInputEl.value) / 100;
+  const tipAmount = calculateTip(btnNumVal);
+  const totalBill = calculateBill(tipAmount);
+  if (Number(numOfPeople.value)) {
+    tips_amount.innerHTML = tipAmount ? `$${tipAmount.toFixed(2)}` : ` $${0.0}`;
+    total_amount.innerHTML = totalBill
+      ? `$${totalBill.toFixed(2)}`
+      : ` $${0.0}`;
+    activateIfNumIsNotZero();
+  } else {
+    numOfPeople.classList.toggle("num-zero");
+    numZeroDisplay.classList.toggle("num-display");
+  }
+});
+console.log(btnReset);
+btnReset.addEventListener('click', function(){
+  valInputEl.value = '';
+  tips_amount.innerHTML = `$0.00`
+  total_amount.innerHTML = `$0.00`;
+  numOfPeople.value= ''
+  bill.value = '';
+}) 
